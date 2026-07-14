ac1fba60e4a705a69152f0b7f7b58873264bc850
22born <saroshsamir22@gmail.com>
2026-07-14 17:11:12 +0100

    Update B.kt

--- B.kt
+++ B.kt
@@ -1,1 +1,33 @@
+Task
+Implement:
+class MedicationScheduleResolver {
+    fun resolve(request: ScheduleRequest): ScheduleResult
+}
+The exact internal representation is up to you, but the public contract must support:
+data class ScheduleRequest(
+    val medications: List<MedicationRule>,
+    val constraints: List<SeparationConstraint>,
+    val rangeStart: ZonedDateTime,
+    val rangeEndExclusive: ZonedDateTime,
+    val defaultZone: ZoneId,
+    val travelWindows: List<TravelWindow> = emptyList()
+)
 
+data class ScheduleResult(
+    val doses: List<ScheduledDose>,
+    val diagnostics: List<String>
+)
+
+data class ScheduledDose(
+    val medicationId: String,
+    val scheduledAt: ZonedDateTime,
+    val dosage: String,
+    val status: DoseStatus
+)
+
+enum class DoseStatus {
+    Scheduled,
+    SkippedDueToPause,
+    SkippedDueToConflict,
+    NeedsReview
+}
